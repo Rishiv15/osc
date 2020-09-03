2587904225b8a1b01aa5635b1839a62ddbbc807c
rishiv <rishivchhabria@gmail.com>
2020-09-03 06:31:15 -0700

    temp

--- register.php
+++ register.php
@@ -3,65 +3,68 @@
 ?>
 
     <main>
-        <form class="needs-validation" novalidate>
-            <div class="form-row">
-                <div class="col-md-6 mb-3">
-                    <label for="validationTooltip01">First name</label>
-                    <input type="text" class="form-control" id="validationTooltip01" value="Mark" required>
-                    <div class="valid-tooltip">
-                        Looks good!
+        <div class="container">
+            <form class="needs-validation" novalidate>
+                <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <label for="validationTooltip01">First name</label>
+                        <input type="text" class="form-control" id="validationTooltip01" value="Mark" required>
+                        <div class="valid-tooltip">
+                            Looks good!
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="validationTooltip02">Last name</label>
+                        <input type="text" class="form-control" id="validationTooltip02" value="Otto" required>
+                        <div class="valid-tooltip">
+                            Looks good!
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-6 mb-3">
-                    <label for="validationTooltip02">Last name</label>
-                    <input type="text" class="form-control" id="validationTooltip02" value="Otto" required>
-                    <div class="valid-tooltip">
-                        Looks good!
+                <div class="form-row">
+                    <div class="col-md-6 mb-3">
+                        <label for="validationTooltip03">City</label>
+                        <input type="text" class="form-control" id="validationTooltip03" required>
+                        <div class="invalid-tooltip">
+                            Please provide a valid city.
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <label for="validationTooltip04">State</label>
+                        <select class="custom-select" id="validationTooltip04" required>
+                            <option selected disabled value="">Choose...</option>
+                            <option>...</option>
+                        </select>
+                        <div class="invalid-tooltip">
+                            Please select a valid state.
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <label for="validationTooltip05">Zip</label>
+                        <input type="text" class="form-control" id="validationTooltip05" required>
+                        <div class="invalid-tooltip">
+                            Please provide a valid zip.
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="form-row">
-                <div class="col-md-6 mb-3">
-                    <label for="validationTooltip03">City</label>
-                    <input type="text" class="form-control" id="validationTooltip03" required>
-                    <div class="invalid-tooltip">
-                        Please provide a valid city.
-                    </div>
-                </div>
-                <div class="col-md-3 mb-3">
-                    <label for="validationTooltip04">State</label>
-                    <select class="custom-select" id="validationTooltip04" required>
-                        <option selected disabled value="">Choose...</option>
-                        <option>...</option>
+                <div class="mb-3">
+                    <select class="custom-select" required>
+                        <option value="">Blood Group</option>
+                        <option value="A+">A+</option>
+                        <option value="A-">A-</option>
+                        <option value="B+">B+</option>
+                        <option value="B-">B-</option>
+                        <option value="O+">O+</option>
+                        <option value="O-">O-</option>
+                        <option value="AB+">AB+</option>
+                        <option value="AB-">AB-</option>
                     </select>
-                    <div class="invalid-tooltip">
-                        Please select a valid state.
-                    </div>
+                    <div class="invalid-feedback">Example invalid custom select feedback</div>
                 </div>
-                <div class="col-md-3 mb-3">
-                    <label for="validationTooltip05">Zip</label>
-                    <input type="text" class="form-control" id="validationTooltip05" required>
-                    <div class="invalid-tooltip">
-                        Please provide a valid zip.
-                    </div>
-                </div>
-            </div>
-            <div class="mb-3">
-                <select class="custom-select" required>
-                    <option value="">Blood Group</option>
-                    <option value="A+">A+</option>
-                    <option value="A-">A-</option>
-                    <option value="B+">B+</option>
-                    <option value="B-">B-</option>
-                    <option value="O+">O+</option>
-                    <option value="O-">O-</option>
-                    <option value="AB+">AB+</option>
-                    <option value="AB-">AB-</option>
-                </select>
-                <div class="invalid-feedback">Example invalid custom select feedback</div>
-            </div>
-            <button class="btn btn-primary" type="submit" name="register-submit">Submit form</button>
-        </form>
+                <button class="btn btn-primary" type="submit" name="register-submit">Submit form</button>
+            </form>
+        </div>
+        
     </main>
 
 <?php  
